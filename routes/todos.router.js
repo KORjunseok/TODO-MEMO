3bb5882612d17dc4371fbc06f7a24ba1855e4aab
Oh Junseok <y21zzp@gmail.com>
2023-07-12 20:34:27 +0900

    할 일 메모사이트 완성

--- routes/todos.router.js
+++ routes/todos.router.js
@@ -19,4 +19,72 @@ router.post("/todos", async (req, res) => {
   res.send({ todo });
 });
 
+router.get("/todos", async (req, res) => {
+  const todos = await Todo.find().sort("-order").exec();
+
+  res.send({ todos });
+});
+
+router.patch("/todos/:todoId", async (req, res) => {
+  const { todoId } = req.params;
+  const { order } = req.body;
+
+  const currentTodo = await Todo.findById(todoId);
+  if (!currentTodo) {
+    throw new Error("존재하지 않는 todo 데이터입니다.");
+  }
+
+  if (order) {
+    const targetTodo = await Todo.findOne({ order }).exec();
+    if (targetTodo) {
+      targetTodo.order = currentTodo.order;
+      await targetTodo.save();
+    }
+    currentTodo.order = order;
+  }
+
+  await currentTodo.save();
+
+  res.send({});
+});
+
+router.delete("/todos/:todoId", async (req, res) => {
+  const { todoId } = req.params;
+
+  const currentTodo = await Todo.findById(todoId);
+  if (!currentTodo) {
+    res.status(400).json({ errormessage: "존재하지 않는 할 일입니다." });
+  } else {
+    await Todo.deleteOne({ _id: todoId });
+    res.status(200).json({ message: "할 일 삭제 완료!" });
+  }
+});
+
+router.patch("/todos/:todoId", async (req, res) => {
+  const { todoId } = req.params;
+  const { order, value, done } = req.body;
+
+  const currentTodo = await Todo.findById(todoId).exec();
+  if (!currentTodo) {
+    throw new Error("존재하지 않는 todo 데이터입니다.");
+  }
+
+  if (order) {
+    const targetTodo = await Todo.findOne({ order }).exec();
+    if (targetTodo) {
+      targetTodo.order = currentTodo.order;
+      await targetTodo.save();
+    }
+    currentTodo.order = order;
+  } else if (value) {
+    currentTodo.value = value;
+  } else if (done !== undefined) {
+    currentTodo.doneAt = done ? new Date() : null;
+  }
+
+  await currentTodo.save();
+
+  res.send({});
+});
+
 module.exports = router;
